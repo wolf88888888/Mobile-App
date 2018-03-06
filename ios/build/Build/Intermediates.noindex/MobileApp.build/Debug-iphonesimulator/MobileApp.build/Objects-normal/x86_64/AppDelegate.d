03c279945ff8ed56bf257930c3ab111a5eb5912a
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2018-03-06 17:30:36 -0600

    implement react-native-smart-splash-screen for iOS

--- ios/build/Build/Intermediates.noindex/MobileApp.build/Debug-iphonesimulator/MobileApp.build/Objects-normal/x86_64/AppDelegate.d
+++ ios/build/Build/Intermediates.noindex/MobileApp.build/Debug-iphonesimulator/MobileApp.build/Objects-normal/x86_64/AppDelegate.d
@@ -9,4 +9,5 @@ dependencies: \
   /Users/ricardodalessandro/Documents/RGD_Solutions/LockChain/Mobile-App/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
   /Users/ricardodalessandro/Documents/RGD_Solutions/LockChain/Mobile-App/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
   /Users/ricardodalessandro/Documents/RGD_Solutions/LockChain/Mobile-App/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
-  /Users/ricardodalessandro/Documents/RGD_Solutions/LockChain/Mobile-App/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h
+  /Users/ricardodalessandro/Documents/RGD_Solutions/LockChain/Mobile-App/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h \
+  /Users/ricardodalessandro/Documents/RGD_Solutions/LockChain/Mobile-App/ios/../node_modules/react-native-smart-splash-screen/ios/RCTSplashScreen/RCTSplashScreen/RCTSplashScreen.h
