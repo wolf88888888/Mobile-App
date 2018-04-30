309657d9477aa8375bd2743345fcb1a1f51ac184
Kristiyan Serafimov <kristian.plamenov@gmail.com>
2018-04-30 18:05:21 +0300

    fix, feat: Welcome screen styles, create button component, keyboard hide

--- src/components/common/SmartInput.js
+++ src/components/common/SmartInput.js
@@ -32,9 +32,9 @@ const styles = StyleSheet.create({
     },
     input: {
         flex: 1,
-        marginLeft: 20,
-        marginRight: 20,
+        marginHorizontal: 20,
         color: '#fff',
+        marginBottom: -4,
         fontSize: 17,
         fontFamily: 'FuturaStd-Light'
     }
@@ -89,8 +89,9 @@ class SmartInput extends Component {
             <View style={[styles.container]}>
                 <TextInput
                     ref={(i) => { this.input = i; }}
-                    underlineColorAndroid="#e4a193"
                     style={styles.input}
+                    underlineColorAndroid="rgba(0,0,0,0)"
+                    selectionColor="white"
                     {...this.props}
                 />
 
