8eaac5fe4c99511776605544c06829d240fe7f54
Kristiyan Pamidov <chrispam@students.softuni.bg>
2018-05-10 16:16:57 +0300

    Exported GetStartedImage (in welcome screens) as atom

---
+++ src/components/atoms/GetStartedImage/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    lowOpacity: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end'
+    },
+    getStartedImage: {
+        width: 400,
+        height: 80
+    }
+});
+
+
+export default styles;
