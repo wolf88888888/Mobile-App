499aa52432c98f5b884662b80d0dd75aecfa76f6
yuri ys <yuri930.sw@gmail.com>
2018-06-16 06:10:23 +0800

    commit

--- src/components/screens/MyTrips/index.js
+++ src/components/screens/MyTrips/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View, FlatList,TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import DialogProgress from 'react-native-dialog-progress'
 import Requester, { getMyHotelBookings, getUserInfo } from '../../../utils/requester';
@@ -35,63 +35,17 @@ class MyTrips extends Component {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
-                 
-                   <View style={styles.chatToolbar}>
-                                    <TouchableOpacity onPress={this.onBackPress}>
-                                        <Image style={styles.btn_backImage} source={require('../../../../src/assets/icons/icon-back-white.png')}/>
-                                    </TouchableOpacity>
-
-                                    <Text style={styles.title}>Your Trip</Text>
-                                   
-                                  
-                     </View>
-
-
-                
-            <View>
-               <FlatList style={styles.flatList}
-                data="none"
-                renderItem={
-                    ({item}) =>
-                        <View style={styles.Listcontainer} >
-                            <View style={{flex: 1, flexDirection: 'row'}}>
-                                <View style={styles.img_round}><Text style={styles.innertext}>25</Text><Text style={{color:'#fff'}}>jan</Text></View>
-                                               
-                                <View style={styles.flatList}> 
-                                    <View>
-                                        <Text style={styles.subtext}>2:00pm</Text>
-                                        <Text style={styles.subtitle}>Check into Garden Loft Apartment</Text> 
-                                    </View>
-
-
-                                    <View style={styles.imageViewWrapper}>
-
-                                    <View style={styles.placeholderImageView}>
-                                        <Image
-                                            style={styles.placeholderImage}
-                                            source={require('../../../assets/apartment.png')}
-                                        />
-                                    </View>
-                                    <View style={{flex: 1, flexDirection: 'row'}}>
-                                   
-                                        <View>
-                                           <Text style={styles.subtext}>Bargo Pinti 50121</Text>
-                                           <Text style={styles.subtext}>Firenze Italy</Text>
-                                        </View>
-                                        <View>
-                                           <Image style={styles.senderImage} source={require('../../../../src/assets/icons/senderImages.png')}/>
-                                        </View>
-                                    </View>
-                                    </View>
-
-                                </View>
-                            </View>
-                        </View> 
-
-                        }
+                <View style={styles.placeholderImageView}>
+                    <Image
+                        style={styles.placeholderImage}
+                        source={require('../../../assets/placeholder_mytrips.png')}
                     />
-            </View>
-               
+                </View>
+                <Text style={styles.title}>You have no upcoming trips</Text>
+                <Text style={styles.subtext}>Discover your next experience</Text>
+                <TouchableOpacity onPress={this.onStartExploring} style={styles.buttonExplore}>
+                    <Text style={styles.exploreBtnText}>Start Exploring</Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -121,9 +75,6 @@ class MyTrips extends Component {
             console.log(err);
         });
         //this.props.navigation.navigate('EXPLORE');
-    }
-    onBackPress = () => {
-        this.props.navigation.navigate('EXPLORE');
     }
 }
 
