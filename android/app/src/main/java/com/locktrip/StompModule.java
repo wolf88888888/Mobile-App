674b1e5bde92034a99029e3e0fadec2c36e19c09
yuri ys <yuri930.sw@gmail.com>
2018-09-17 04:01:09 +0800

    integrate fixed native library to react native

--- android/app/src/main/java/com/locktrip/StompModule.java
+++ android/app/src/main/java/com/locktrip/StompModule.java
@@ -33,12 +33,61 @@ import static com.facebook.react.bridge.UiThreadUtil.runOnUiThread;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 
 public class StompModule extends ReactContextBaseJavaModule {
-
-    static final String _url = "wss://beta.locktrip.com/socket";
+    final String TAG = "StompModule";
+    String _url = "wss://beta.locktrip.com/socket";
     private Context context;
     private WebSocketStompClient _client = null;
     StompSession _session = null;
     StompSession.Subscription _lastSubscription = null;
+
+    Callback _callbackConnection = null;
+    Callback _messageConnection = null;
+
+    StompSessionHandler _sessionHandler = new StompSessionHandler() {
+        @Override
+        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+            Log.e(TAG, "Connected~~~~");
+            _session = session;
+            _callbackConnection.invoke(null, true);
+            if (_isOnce) {
+                MainActivity.this.getData();
+            }
+        }
+
+        @Override
+        public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+            Log.e(TAG, "handleException~~~~");
+            disconnect();
+            exception.printStackTrace();
+        }
+
+        @Override
+        public void handleTransportError(StompSession session, Throwable exception) {
+            Log.e(TAG, "handleTransportError~~~~");
+            disconnect();
+            if (exception instanceof ConnectionLostException) {
+                // if connection lost, call this
+            }
+            else if (exception instanceof DeploymentException) {
+                // if connection failed, call this
+            }
+            else {
+                //unknown issues
+            }
+
+            exception.printStackTrace();
+        }
+
+        @Override
+        public Type getPayloadType(StompHeaders headers) {
+            return String.class;
+        }
+
+        @Override
+        public void handleFrame(StompHeaders headers, Object payload) {
+            Log.e(TAG, payload.toString());
+        }
+    };
 
     public StompModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -102,9 +151,30 @@ public class StompModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void connect(Callback ){
-
+    public void connect(String url, Callback callbackConnection){
+        this._url = url;
+        this._callbackConnection = callbackConnection;
+        this.connect();
     }
+
+    @ReactMethod
+    public void getData(String message, String destination, Callback messageCallback) {
+        // _message = "{\"uuid\":\"e38effa6-491f-4e9e-b3b4-e4a2f71ed835\",\"query\":\"?region=52612&currency=EUR&startDate=15/09/2018&endDate=16/09/2018&rooms=%5B%7B%22adults%22:2,%22children%22:%5B%5D%7D%5D\"}";
+        // _destination = "search/e38effa6-491f-4e9e-b3b4-e4a2f71ed835";
+        this._message = message;
+        this._destination = destination;
+        this._messageConnection = messageCallback;
+
+        this.subscription();
+    }
+
+    @ReactMethod
+    public void close() {
+        this._callbackConnection = null;
+        this._messageCallback = null;
+        this.unSubscription();
+    }
+
 
     @ReactMethod
     public void startSession(String uid, String query, Callback success) {
