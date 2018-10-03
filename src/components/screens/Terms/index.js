dbf800aecccf4b46653537bb4cb0dae686952d76
yuri ys <yuri930.sw@gmail.com>
2018-10-04 03:07:22 +0800

    remove create wallet

--- src/components/screens/Terms/index.js
+++ src/components/screens/Terms/index.js
@@ -1,6 +1,9 @@
+import React, {Component} from 'react';
 import { StatusBar, Text, TouchableOpacity, View } from 'react-native';
 import { NavigationActions, StackActions } from 'react-navigation';
-import React, {Component} from 'react';
+import ProgressDialog from '../../atoms/SimpleDialogs/ProgressDialog';
+import requester from '../../../initDependencies';
+import { PUBLIC_URL } from '../../../config';
 
 import styles from './styles';
 
@@ -8,13 +11,13 @@ class Terms extends Component {
 
     constructor(props) {
         super(props);
-        this.onDecline = this.onDecline.bind(this);
+
+        this.state = {
+            showProgress: false
+        }
     }
 
-    componentDidMount() {
-    }
-
-    onDecline() {
+    onDecline = () => {
 		let resetAction = StackActions.reset({
 			index: 0,
 			actions: [
@@ -24,8 +27,41 @@ class Terms extends Component {
 		this.props.navigation.dispatch(resetAction);
     }
 
+    onNext = () => {
+        // this.props.navigation.navigate('CreateWallet', { ...params });
+        const { params } = this.props.navigation.state;
+        const {navigate} = this.props.navigation;
+        let user = params;
+        user['image'] = PUBLIC_URL + "images/default.png";
+
+        console.log(user);
+
+        this.setState({ showProgress: true });
+        requester.register(user, null).then(res => {
+            this.setState({ showProgress: false });
+            if (res.success) {
+                console.log(res);
+                navigate('CongratsWallet')
+            } else {
+                res.errors.then(data => {
+                    const { errors } = data;
+                    Object.keys(errors).forEach((key) => {
+                        if (typeof key !== 'function') {
+                            Toast.showWithGravity(errors[key].message, Toast.SHORT, Toast.BOTTOM);
+                            console.log('Error logging in:', errors[key].message);
+                        }
+                    });
+                });
+            }
+        })
+        .catch(err => {
+            this.setState({ showProgress: false });
+            Toast.showWithGravity('Cannot get messages, Please check network connection.', Toast.SHORT, Toast.BOTTOM);
+            console.log(err);
+        });
+    }
+
     render() {
-        const { navigate } = this.props.navigation;
         const { params } = this.props.navigation.state;
         console.log("Terms params", params);
         return (
@@ -40,7 +76,7 @@ class Terms extends Component {
                 <Text style={styles.paragraph}>I understand that if I forget my wallet password, the only way to recover it would be through the mnemonic keywords provided during the wallet creation. It is my sole responsibility to write them and store them in a safe place. I also understand the dangers associated with Blockchain based assets and under no circumstances will I hold LockTrip responsible for any loss that could arise due to any type of security breach and/or forgotten wallet password or mnemonic keywords.</Text>
 
                 <View style={styles.buttonsView}>
-                    <TouchableOpacity onPress={() => navigate('CreateWallet', { ...params })}>
+                    <TouchableOpacity onPress={ this.onNext }>
                         <View style={styles.acceptButtonView}>
                             <Text style={styles.acceptButtonText}>Accept</Text>
                         </View>
@@ -52,6 +88,13 @@ class Terms extends Component {
                         </View>
                     </TouchableOpacity>
                 </View>
+                <ProgressDialog
+                    visible={this.state.showProgress}
+                    title=""
+                    message="Registering..."
+                    animationType="slide"
+                    activityIndicatorSize="large"
+                    activityIndicatorColor="black"/>
             </View>
         );
     }
