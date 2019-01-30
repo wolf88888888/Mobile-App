265f801e29224787e630b6a39ddff7c55635dd2b
yuri930 <yuri930.sw@gmail.com>
2019-01-30 10:44:04 +0200

    LM-115(iOS)
    Fix App for iOS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './src/components/app/App';
+// import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- ios/Locktrip/AppDelegate.m
+++ ios/Locktrip/AppDelegate.m
@@ -9,6 +9,9 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RCTSplashScreen.h" //import interface
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+@import GoogleMaps;
 
 @implementation AppDelegate
 
@@ -22,7 +25,13 @@
                                                       moduleName:@"Locktrip"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [UIColor blackColor];
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                           didFinishLaunchingWithOptions:launchOptions];
+  [GMSServices provideAPIKey:@"AIzaSyDqHNX83ZkYeqikdpHP4QKpOVbKAX4Vtts"];
+  //[RCTSplashScreen open:rootView];
+  [RCTSplashScreen open:rootView withImageNamed:@"splash"]; // activate splashscreen, imagename from LaunchScreen.xib
+  
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.85 green:0.48 blue:0.38 alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
@@ -32,4 +41,16 @@
   return YES;
 }
 
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                                openURL:url
+                                                      sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                                                             annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+                  ];
+  // Add any custom logic here.
+  return handled;
+}
 @end
